898e7030c55f930df15b78656a4d2526744df8e4
Harsh Gupta <1045588+hgupta@users.noreply.github.com>
2018-02-02 01:51:28 +0530

    moved to separate gh repo

--- crypto-engines/index.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto')
-const reduce = require('./reducer')
-
-const ranf = (len = 16) =>
-  reduce(crypto.randomBytes(len > 16 || len < 0 ? 16 : len))
-
-module.exports = exports = ranf
--- crypto-engines/reducer.js
+++
@@ -1,6 +0,0 @@
-const reduce = (n, base = 256, pow = 0, acc = 0) =>
-  n.length <= pow
-    ? acc
-    : reduce(n, base * base, pow + 1, acc + Number(n[pow] / base))
-
-module.exports = exports = reduce
