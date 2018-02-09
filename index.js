274a0fcc885bfbca685f0734d71071ef468d5978
Harsh Gupta <1045588+hgupta@users.noreply.github.com>
2018-02-10 01:20:05 +0530

    for rollup

---
+++ index.js
@@ -0,0 +1,18 @@
+const bernoulli = require('./src/bernoulli')
+const binomial = require('./src/binomial')
+const cauchy = require('./src/cauchy')
+const chisquare = require('./src/chisquare')
+const exponential = require('./src/exponential')
+const gamma = require('./src/gamma')
+const laplace = require('./src/laplace')
+const lognormal = require('./src/lognormal')
+const normal = require('./src/normal')
+const poisson = require('./src/poisson')
+const uniform = require('./src/uniform')
+
+/* eslint-disable */
+module.exports = exports = {
+  bernoulli, binomial, cauchy, chisquare, exponential, gamma, laplace,
+  lognormal, normal, poisson, uniform
+}
+/* eslint-enable */
